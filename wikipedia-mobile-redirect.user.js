cbe938eea2c4afc6d0b795914c75e1ccfa3272d7
Michael Norris <michael.norris93@gmail.com>
2015-07-07 19:14:24 -0400

    Update wikipedia-mobile-redirect.user.js
    Fixed incorrect downloadURL meta descriptor

--- wikipedia-mobile-redirect.user.js
+++ wikipedia-mobile-redirect.user.js
@@ -2,11 +2,11 @@
 // @name       Wikipedia Mobile Redirect
 // @author     Michael Norris
 // @namespace  http://norrism.com
-// @version    1.0.0
+// @version    1.0.1
 // @description  Redirects Mobile Wikipedia Links to Desktop Site
 // @match      http://*.m.wikipedia.org/*
 // @match      https://*.m.wikipedia.org/*
-// @downloadURL    https://github.com/norrism/wikipedia-mobile-redirect/raw/master/wikipedia-mobile-redirect.js
+// @downloadURL    https://github.com/norrism/wikipedia-mobile-redirect/raw/master/wikipedia-mobile-redirect.user.js
 // @run-at    document-start
 // ==/UserScript==
 
